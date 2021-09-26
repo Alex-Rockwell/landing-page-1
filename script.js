5c0ac68db709078a3df124f75bacfad345bad328
Alex-Rockwell <vin-ag@list.ru>
2021-09-26 12:16:21 +0400

    21.09.26

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 ///////////////////////////////
 // Add shadow to header
 
-const header = document.querySelector('.header');
+const header = document.querySelector('.header')
 
 window.addEventListener('scroll', function() {
     if (window.scrollY > 0) {
-      header.classList.add('is-scrolled');
+      header.classList.add('is-scrolled')
     } else {
-      header.classList.remove('is-scrolled');
+      header.classList.remove('is-scrolled')
     }  
-});
+})
+
+///////////////////////////////
+// Lazy loading images
+
+const images = document.querySelectorAll('[data-src]')
+const imageOptions = {
+  threshold: 0,
+  rootMargin: '0px 0px 300px 0px'
+}
+
+const imageObserver = new IntersectionObserver((entries, imageObserver) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return
+    } else {
+      preloadImg(entry.target)
+      imageObserver.unobserve(entry.target)
+    }
+  })
+}, imageOptions)
+
+images.forEach(img => {
+  imageObserver.observe(img)
+})
+
+function preloadImg(img) {
+  const src = img.getAttribute('data-src')
+  img.src = src
+}
